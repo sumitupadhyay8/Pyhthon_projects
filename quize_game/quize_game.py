150c11a9536efdbe432e1fe6a2b5740db4940b1a
Sumit <officially.sumit.upadhyay@gmail.com>
2024-03-05 01:55:46 +0530

    A basic quize game structure

---
+++ quize_game/quize_game.py
@@ -0,0 +1,18 @@
+print("Welconme  to my computer quze")
+playing = input("Would you like to play this game?\n")
+
+if playing.upper() == "NO":
+    print("Any wasys have a nice Day  :)")
+    
+elif playing.upper() == "YES":
+    print("Okey Lets play : ) ")
+
+    name=input("What is your name")
+    
+    a1="Does ",name,"is the person you want to become?"
+    if a1 == "yes":
+        print("That means you are confident about yourself ")
+        
+
+else:
+    print("Sorry I did't get your answer may be there is a typo : /")
